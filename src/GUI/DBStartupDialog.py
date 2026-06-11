40a1e9ec4613bafcfd098b8171f7cfc3dcd56317
wachin <wachin.id@gmail.com>
2026-06-11 12:04:47 -0500

    fix(startup): use palette link color in welcome dialog
    - Add _linkColour() and _setWelcomeText() to DBStartupDialog.py.
    
    - Inject active palette link color into startup label HTML to
    
      replace hard-coded dark blue from generated UI file.
    
    - Enable setOpenExternalLinks(True) for consistent link behavior.
    
    - Verified via py_compile and offscreen Qt check (color:#569cd6
    
      in dark mode).
    
    - Pattern ready for centralization across remaining dialogs.

--- src/GUI/DBStartupDialog.py
+++ src/GUI/DBStartupDialog.py
@@ -22,7 +22,7 @@ Created on 17 Apr 2011
 @author: Mike Thomas
 '''
 from GUI.ui_dbStartup import Ui_dbStartup
-from PyQt5.QtCore import QSettings
+from PyQt5.QtCore import QSettings, Qt
 from PyQt5.QtWidgets import QDialog
 
 
@@ -30,11 +30,47 @@ class DBStartupDialog(QDialog, Ui_dbStartup):
     def __init__(self, version, parent=None):
         super(DBStartupDialog, self).__init__(parent)
         self.setupUi(self)
+        self._setWelcomeText()
         self.setWindowTitle(self.tr("Welcome to DrumBurp v") + version)
         self.buttonBox.button(self.buttonBox.Ok).setFocus()
         settings = QSettings()
         self.hideOnStartupCheckBox.setChecked(
             bool(settings.value("HideStartupDialog", False, type=bool)))
+
+    def _linkColour(self):
+        return self.palette().color(self.palette().Link).name()
+
+    def _setWelcomeText(self):
+        link_colour = self._linkColour()
+        self.label.setTextFormat(Qt.RichText)
+        self.label.setOpenExternalLinks(True)
+        self.label.setWordWrap(True)
+        self.label.setTextInteractionFlags(Qt.TextBrowserInteraction)
+        self.label.setText(
+            '<html><head/><body>'
+            '<p><span style=" font-size:10pt; font-weight:600; '
+            'font-style:italic;">'
+            + self.tr('DrumBurp Copyright (C) 2011-16 Michael Thomas')
+            + '</span></p>'
+            '<p><br/></p>'
+            '<p>'
+            + self.tr(
+                'This program comes with ABSOLUTELY NO WARRANTY; for details '
+                'see the <span style=" font-weight:600;">Help&gt;About '
+                'DrumBurp</span> menu item. This is free software, and you '
+                'are welcome to redistribute it under certain conditions; '
+                'see the licensing information in <span style=" '
+                'font-weight:600;">Help&gt;About DrumBurp</span> for details.')
+            + '</p>'
+            '<p><br/></p>'
+            '<p>'
+            + self.tr('Support and further licensing information is available at ')
+            + '<a href="http://www.whatang.org">'
+            + '<span style=" text-decoration: underline; color:%s;">'
+            % link_colour
+            + 'www.whatang.org</span></a>'
+            + '</p></body></html>'
+        )
 
     def accept(self):
         settings = QSettings()
